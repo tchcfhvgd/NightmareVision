40c63cd58299f9390808ad50c7980d4d6615abb3
tchcfhvgd <99186628+tchcfhvgd@users.noreply.github.com>
2024-11-03 11:51:43 +0800

    Update AtlasFrameMaker.hx

--- source/gameObjects/animateatlas/AtlasFrameMaker.hx
+++ source/gameObjects/animateatlas/AtlasFrameMaker.hx
@@ -16,13 +16,8 @@ import meta.states.*;
 import flixel.graphics.FlxGraphic;
 import flixel.graphics.frames.FlxFramesCollection;
 import flixel.graphics.frames.FlxFrame;
-#if desktop
 import sys.FileSystem;
 import sys.io.File;
-#else
-import js.html.FileSystem;
-import js.html.File;
-#end
 
 using StringTools;
 class AtlasFrameMaker extends FlxFramesCollection
